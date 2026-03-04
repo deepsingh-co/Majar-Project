e2e679a8195ac15abf886cff8c5708f0627e0582
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-03-04 16:25:13 +0530

    styling the add page

--- app.js
+++ app.js
@@ -18,12 +18,11 @@ main().then(() =>{
 async function main(){
     await mongoose.connect(MONGO_URL);
 }
-
+app.engine("ejs" , ejsMate);
 app.set('view engine' , 'ejs');
 app.set('views' , path.join(__dirname , 'views'));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride('_method'));
-app.engine("ejs" , ejsMate);
 app.use(express.static(path.join(__dirname , "public")));
 
 app.get('/', (req , res) =>{
